9f1340e44508881ca1bcb049823f3a87fc0c84f5
calyn05 <67522453+calyn05@users.noreply.github.com>
2024-06-09 23:40:26 +0300

    added timezone offset

--- components/invites/pieces/timeRemaining.tsx
+++ components/invites/pieces/timeRemaining.tsx
@@ -22,8 +22,7 @@ export default function TimeRemaining({
 
   const now = Date.now();
 
-  const timeRemaining = dateInMs - now;
-  // + options?.timezoneOffset; // Essential for pg admin date format
+  const timeRemaining = dateInMs - now + options?.timezoneOffset; // Essential for pg admin date format
   const remainigDays = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
   const remainigHours = Math.floor(
     (timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
