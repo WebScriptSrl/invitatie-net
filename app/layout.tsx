0ca3821de8e671d22643555067e788318c49ea36
calyn05 <67522453+calyn05@users.noreply.github.com>
2024-06-10 03:30:19 +0300

    added metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,77 @@
 import type { Metadata } from "next";
 import { lora } from "@/styles/fonts";
 import "@/styles/globals.css";
+import { ensureStartsWith } from "@/lib/utils";
 
-export const metadata: Metadata = {
-  title: "Invitatie Net",
-  description: "Invitatii online personalizate pentru orice eveniment special.",
-};
+const { TWITTER_CREATOR, TWITTER_SITE, SITE_NAME, COMPANY_NAME } = process.env;
+
+const baseUrl = process.env.PUBLIC_BASE_URL
+  ? `https://${process.env.PUBLIC_BASE_URL}`
+  : `http://localhost:3000`;
+
+const twitterCreator = TWITTER_CREATOR
+  ? ensureStartsWith(TWITTER_CREATOR, "@")
+  : undefined;
+
+const twitterSite = TWITTER_SITE
+  ? ensureStartsWith(TWITTER_SITE, "@")
+  : undefined;
+
+export async function generateMetadata(): Promise<Metadata> {
+  // const metadata = await getMetadata("rootLayout");
+
+  return {
+    metadataBase: new URL(baseUrl),
+    title: {
+      default: "Invitatie Net",
+      template: `%s | ${SITE_NAME}`,
+    },
+    // to be continued
+    // robots: {
+    //   follow: true,
+    //   index: true,
+    // },
+    description: "Invitatii online personalizate pentru evenimente speciale.",
+    authors: [
+      {
+        name: COMPANY_NAME,
+      },
+    ],
+
+    openGraph: {
+      title: "Invitatie Net",
+      description: "Invitatii online personalizate pentru evenimente speciale.",
+      type: "website",
+      locale: "ro_RO",
+      url: baseUrl,
+      siteName: SITE_NAME,
+      images: [
+        {
+          url: "/og-image.jpg",
+          width: 1200,
+          height: 630,
+          alt: "Invitatie Net og image",
+        },
+      ],
+    },
+
+    twitter: {
+      card: "summary_large_image",
+      site: twitterSite,
+      creator: twitterCreator,
+      title: "Invitatie Net",
+      description: "Invitatii online personalizate pentru evenimente speciale.",
+      images: [
+        {
+          url: "/og-image.jpg",
+          width: 1200,
+          height: 630,
+          alt: "Invitatie Net og image",
+        },
+      ],
+    },
+  };
+}
 
 export default function RootLayout({
   children,
--- lib/utils.ts
+++ lib/utils.ts
@@ -6,11 +6,10 @@ export function cn(...inputs: ClassValue[]) {
   return clsx(...inputs);
 }
 
-export const ensureStartsWith = (stringToCheck: string, startsWith: string) => {
+export const ensureStartsWith = (stringToCheck: string, startsWith: string) =>
   stringToCheck.startsWith(startsWith)
     ? stringToCheck
     : `${startsWith}${stringToCheck}`;
-};
 
 export const toDateString = (date: Date) => {
   return new Date(date).toLocaleDateString("ro-RO", {
